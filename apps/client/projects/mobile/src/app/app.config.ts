df401e23a54ec2fd258340f1ca0eede4b8489240
Ange <93579219+Ange230700@users.noreply.github.com>
2026-03-19 17:58:21 +0000

    chore(apps/client): add Ionic to the mobile Angular app

--- apps/client/projects/mobile/src/app/app.config.ts
+++ apps/client/projects/mobile/src/app/app.config.ts
@@ -1,11 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideIonicAngular({})
   ]
 };
