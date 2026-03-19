15b0144ae828d093faced9ceb74448abf36755ac
Ange <93579219+Ange230700@users.noreply.github.com>
2026-03-19 18:11:42 +0000

    chore(apps/client):  initialize Capacitor for the mobile target

---
+++ apps/client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.solangebernard.mobile',
+  appName: 'SolangeBernard Mobile',
+  webDir: 'dist/mobile/browser'
+};
+
+export default config;
